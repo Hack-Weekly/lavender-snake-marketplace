0f1504ef5c755be984219562842f45535c3f85a6
Xarunoba <me@xaru.win>
2023-04-23 00:52:05 +0800

    check if user is logged in

--- src/pages/api/item/index.ts
+++ src/pages/api/item/index.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import errorResponse from "~/server/errorResponse";
-import { isItem, itemDb } from "../itemUtils";
+import { isItem, itemDb } from "~/server/itemUtils";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "~/server/auth";
 // POST a new item.
 
 export default async function handler(
@@ -10,23 +12,26 @@ export default async function handler(
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { body } = req;
   if (req.method === "POST") {
-    if (isItem(body)) {
-      const data = {
-        name: body.name,
-        artist: body.artist,
-        description: body.description,
-        image: body.image,
-        price: body.price,
-        seller: body.seller,
-      };
-      try {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        const item = await itemDb.put(data);
-        console.log(item);
-        res.status(200).json(item);
-      } catch (error) {
-        res.status(500).json({ error: error });
-      }
-    } else res.status(500).json({ error: "Invalid Item." });
+    const session = await getServerSession(req, res, authOptions);
+    if (session) {
+      if (isItem(body)) {
+        const data = {
+          name: body.name,
+          artist: body.artist,
+          description: body.description,
+          image: body.image,
+          price: body.price,
+          seller: body.seller,
+        };
+        try {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+          const item = await itemDb.put(data);
+          console.log(item);
+          res.status(200).json(item);
+        } catch (error) {
+          res.status(500).json({ error: error });
+        }
+      } else res.status(500).json({ error: "Invalid Item." });
+    } else res.status(401).json(errorResponse(401));
   } else res.status(405).json(errorResponse(405));
 }
