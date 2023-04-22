341257ce1f4d3e0ad45e2e4122750426572c8c38
Xarunoba <me@xaru.win>
2023-04-22 22:03:08 +0800

    additional comments

--- src/server/deta.ts
+++ src/server/deta.ts
@@ -6,5 +6,10 @@ import { env } from "~/env.mjs";
 //  can easily see, query, update and delete records in the database.
 //  https://deta.space/docs/en/reference/base/sdk
 
+//  Deta Drive is a managed, secure and scalable file storage service with
+//  a focus on end-user simplicity.
+//
+//  https://deta.space/docs/en/reference/drive/sdk
+
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 export const deta = Deta(env.DETA_ACCESS_KEY);
