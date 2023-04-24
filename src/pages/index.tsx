03e8aad77645ac51310da0848e71376508604d5e
Xarunoba <me@xaru.win>
2023-04-25 01:55:59 +0800

    added css class that uses dvh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,23 @@
 import { type GetServerSideProps, type NextPage } from "next";
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 import Hero from "~/components/Hero";
 import { getServerAuthSession } from "~/server/auth";
 import HotBids from "~/components/HotBids";
 import TopArtist from "~/components/TopArtist";
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const session = await getServerAuthSession(ctx);
-  return {
-    props: { session },
-  };
-};
+// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+//   const session = await getServerAuthSession(ctx);
+//   return {
+//     props: { session },
+//   };
+// };
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
 
   return (
     <main
-      className={`min-h-screen bg-white font-playfairDisplay text-primaryText`}
+      className={`min-h-dscreen bg-white font-playfairDisplay text-primaryText`}
     >
       {/* <h1>Welcome to Homepage: {session?.user.name || "Stranger"}!</h1> */}
       <Hero />
