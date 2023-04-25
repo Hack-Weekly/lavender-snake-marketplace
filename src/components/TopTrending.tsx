e2060ad83c3c5cb9572cb5767a551f36f756ab06
ejsadiarin <ejsadiarin@gmail.com>
2023-04-25 13:51:09 +0800

    data add

--- src/components/TopTrending.tsx
+++ src/components/TopTrending.tsx
@@ -2,12 +2,106 @@ import Image from "next/image";
 
 
 type TrendingCardData = {
-  image: string,
+  imageSrc: string,
   title: string,
   author: string,
   bids: number,
   quickBuy: number,
-}
+};
+
+// Categories
+const paintingData: Array<TrendingCardData> = [
+  {
+    imageSrc: "/images/chilly-day.png",
+    title: "Chilly Day",
+    author: "Mike Seraph",
+    bids: 830,
+    quickBuy: 14787,
+  },
+  {
+    imageSrc: "/images/euphoria.png",
+    title: "Euphoria",
+    author: "Ej Kim",
+    bids: 457823,
+    quickBuy: 777123,
+  },
+  {
+    imageSrc: "/images/grand-scheme.png",
+    title: "Grand Scheme",
+    author: "ClearMinds",
+    bids: 98000,
+    quickBuy: 100564,
+  },
+  {
+    imageSrc: "/images/monotone.png",
+    title: "Monotone",
+    author: "MonoRepo",
+    bids: 100,
+    quickBuy: 9000,
+  },
+];
+
+const abstractArtData: Array<TrendingCardData> = [
+  {
+    imageSrc: "/images/geom-world.png",
+    title: "Geom World",
+    author: "Geom Metry",
+    bids: 179,
+    quickBuy: 1890,
+  },
+  {
+    imageSrc: "/images/divergence.png",
+    title: "Divergence",
+    author: "Shadow",
+    bids: 14590,
+    quickBuy: 200000,
+  },
+  {
+    imageSrc: "/images/perfect-grandiose.png",
+    title: "Perfect Grandiose",
+    author: "Jhin Saki",
+    bids: 44,
+    quickBuy: 44000,
+  },
+  {
+    imageSrc: "/images/contrast-dive.png",
+    title: "Contrast Dive",
+    author: "Nvdia Taquia",
+    bids: 1782,
+    quickBuy: 20000,
+  },
+];
+
+const digitalArtData: Array<TrendingCardData> = [
+  {
+    imageSrc: "/images/chilly-day.png",
+    title: "Chilly Day",
+    author: "Mike Seraph",
+    bids: 830,
+    quickBuy: 14787,
+  },
+  {
+    imageSrc: "/images/euphoria.png",
+    title: "Euphoria",
+    author: "Ej Kim",
+    bids: 457823,
+    quickBuy: 777123,
+  },
+  {
+    imageSrc: "/images/grand-scheme.png",
+    title: "Grand Scheme",
+    author: "ClearMinds",
+    bids: 98000,
+    quickBuy: 100564,
+  },
+  {
+    imageSrc: "/images/monotone.png",
+    title: "Monotone",
+    author: "MonoRepo",
+    bids: 100,
+    quickBuy: 9000,
+  },
+];
 
 
 
@@ -19,10 +113,11 @@ export default function TopTrending() {
   );
 } 
 
-function TrendingCard(): TrendingCardData {
+function TrendingCard(category: []) {
   return (
     <div>
       <Image 
+        src={}
         width={250}
         height={250}
         priority
