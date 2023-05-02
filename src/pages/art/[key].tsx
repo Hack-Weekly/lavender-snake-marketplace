024b32466442bc7646a4941f151f8f95866a3c05
ejsadiarin <ejsadiarin@gmail.com>
2023-05-02 18:42:38 +0800

    responsive design till max-lg

--- src/pages/art/[key].tsx
+++ src/pages/art/[key].tsx
@@ -50,8 +50,8 @@ export default function ArtDetailsPage() {
   }
 
   return (
-    <div className="flex gap-7 p-11 justify-center">
-      <div className="flex flex-col ">
+    <div className="flex gap-7 p-11 justify-center items-center max-lg:flex-col">
+      <div className="flex flex-col justify-center-center max-lg:w-full">
         <div className="flex justify-between mb-2">
           <div className="flex rounded-xl bg-grey px-3 py-1">
             <ArrowLeft className="mr-3" />
@@ -62,7 +62,18 @@ export default function ArtDetailsPage() {
             <Share2 />
           </div>
         </div>
-        <div className="rounded-xl border-2 border-black h-96 w-[30rem] text-center">
+
+        {/* for smaller layout */}
+        <div className="hidden max-lg:block max-lg:mb-7 max-lg:mt-3">
+          <h1 className="font-playfairDisplay text-7xl font-bold">{item.name}</h1>
+          <div className="flex mt-6">
+            <h2 className="font-baskervville">Artist: <b>{item.artist}</b></h2>
+            <span className="mx-4">|</span>
+            <h2 className="font-poppins">Category: <b>{item.category}</b></h2>
+          </div>
+        </div>
+
+        <div className="rounded-xl border-2 border-black h-96 w-[30rem] text-center max-lg:w-full">
           image goes here
           {/* TODO: fetch the image seperately from deta drive using the image file name */}
           {/* <img 
@@ -75,69 +86,72 @@ export default function ArtDetailsPage() {
         </div>
         <div className="mt-4">
           <p className="font-poppins font-semibold">DESCRIPTION:</p>
-          <p className="font-playfairDisplay"> {item.description} </p>
+          <p className="font-playfairDisplay max-lg:text-xs"> {item.description} </p>
         </div>
       </div>
+      
+        <div className="flex flex-col gap-5 ml-[2rem] max-lg:w-full">
+          <div className="max-lg:hidden">
+            <h1 className="font-playfairDisplay text-7xl font-bold">{item.name}</h1>
+            <div className="flex mt-6">
+              <h2 className="font-baskervville">Artist: <b>{item.artist}</b></h2>
+              <span className="mx-4">|</span>
+              <h2 className="font-poppins">Category: <b>{item.category}</b></h2>
+            </div>
+          </div>
 
-      <div className="flex flex-col gap-5 ml-[2rem]">
-        <div>
-          <h1 className="font-playfairDisplay text-7xl font-bold">{item.name}</h1>
-          <div className="flex mt-6">
-            <h2 className="font-baskervville">Artist: <b>{item.artist}</b></h2>
-            <span className="mx-4">|</span>
-            <h2 className="font-poppins">Category: <b>{item.category}</b></h2>
+          <div className="flex gap-4 justify-start font-playfairDisplay font-semibold
+                        text-yellowText text-base sm:text-xs md:text-sm lg:text-base
+                          max-lg:flex-col
+          ">
+            <button className="flex justify-center items-center bg-redBg rounded-lg py-2 px-5">
+              <Package className="mr-2" />
+              <p>BID</p>
+            </button>
+            <button className="flex justify-center items-center bg-redBg rounded-lg py-2 px-5 max-lg:order-first">
+              <Truck className="mr-3" />
+              <p>QUICK BUY</p>
+              <span className="mx-4">|</span>
+              <p className="">${item.price}</p>
+            </button>
+            <button className="flex justify-center items-center bg-grey font-bold text-black rounded-lg py-2 px-5">
+              <ShoppingCart className="mr-2" />
+              <p>Add to cart</p>
+            </button>
+          </div>
+
+          {/* Data fetch Bids here, for now just mock data */}
+          <div className="bg-indigoBg font-poppins rounded-lg p-7">
+            <p className="font-bold text-3xl text-yellowText">LATEST BIDS:</p>
+            <div className="flex justify-between m-3 max-lg:text-sm">
+              <div className="flex flex-col justify-center items-center">
+                <p className="text-yellowText font-bold m-3">FROM</p>
+                <div className="flex flex-col justify-center items-start text-grey">
+                  <p>OneMan</p>       
+                  <p>TwoMan</p>
+                  <p>Ej Sadiarin</p>
+                </div>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <p className="text-yellowText font-bold m-3">BIDS</p>
+                <div className="flex flex-col justify-center items-start text-grey">
+                  <p>$10</p>
+                  <p>$100</p>
+                  <p>$1,000</p>
+                </div>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <p className="text-yellowText font-bold m-3">TIME</p>
+                <div className="flex flex-col justify-center items-start text-grey">
+                  <p>1 hour ago</p>
+                  <p>2 hours ago</p>
+                  <p>3 hours ago</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="flex gap-4 justify-between font-playfairDisplay font-semibold text-yellowText text-base sm:text-xs md:text-sm lg:text-base">
-          <button className="flex justify-center items-center bg-redBg rounded-lg py-2 px-5">
-            <Package className="mr-2" />
-            <p>BID</p>
-          </button>
-          <button className="flex justify-center items-center bg-redBg rounded-lg py-2 px-5">
-            <Truck className="mr-3" />
-            <p className="">QUICK BUY</p>
-            <span className="mx-4">|</span>
-            <p className="">${item.price}</p>
-          </button>
-          <button className="flex justify-center items-center bg-grey font-bold text-black rounded-lg py-2 px-5">
-            <ShoppingCart className="mr-2" />
-            <p>Add to cart</p>
-          </button>
-        </div>
-
-        {/* Data fetch Bids here, for now just mock data */}
-        <div className="bg-indigoBg font-poppins rounded-lg p-7">
-          <p className="font-bold text-3xl text-yellowText">LATEST BIDS:</p>
-          <div className="flex justify-evenly m-3">
-            <div className="flex flex-col justify-center items-center">
-              <p className="text-yellowText font-bold m-3">FROM</p>
-              <div className="flex flex-col justify-center items-start text-grey">
-                <p>OneMan</p>            
-                <p>TwoMan</p>            
-                <p>Ej Sadiarin</p>            
-              </div>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <p className="text-yellowText font-bold m-3">BIDS</p>
-              <div className="flex flex-col justify-center items-start text-grey">
-                <p>$10</p>
-                <p>$100</p>
-                <p>$1,000</p>
-              </div>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <p className="text-yellowText font-bold m-3">TIME</p>
-              <div className="flex flex-col justify-center items-start text-grey">
-                <p>1 hour ago</p>
-                <p>2 hours ago</p>
-                <p>3 hours ago</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
     </div>
   );
 }
